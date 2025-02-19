c10f2d4d669d582b778ec79fcecaec9fdc1b4afe
Amir Elbadaoui <amireb51@gmail.com>
2025-02-18 22:42:41 -0500

    [Setup] Added exercise entity

---
+++ backend/src/main/java/com/gymtracker/backend/entity/Exercise.java
@@ -0,0 +1,102 @@
+package com.gymtracker.backend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "exercises")
+public class Exercise {
+	
+	/** The id of the exercise */
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	/** The name of the exercise */
+	private String name;
+	
+	/** The amount of sets performed of the exercise */
+	private int sets;
+	
+	/** The reps performed per set of the exercise */
+	private int reps;
+	
+	/** The weight used for the exercise */
+	private double weight;
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the sets
+	 */
+	public int getSets() {
+		return sets;
+	}
+
+	/**
+	 * @param sets the sets to set
+	 */
+	public void setSets(int sets) {
+		this.sets = sets;
+	}
+
+	/**
+	 * @return the reps
+	 */
+	public int getReps() {
+		return reps;
+	}
+
+	/**
+	 * @param reps the reps to set
+	 */
+	public void setReps(int reps) {
+		this.reps = reps;
+	}
+
+	/**
+	 * @return the weight
+	 */
+	public double getWeight() {
+		return weight;
+	}
+
+	/**
+	 * @param weight the weight to set
+	 */
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+	
+	
+	
+}
